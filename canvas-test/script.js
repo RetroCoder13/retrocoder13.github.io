e0ea9cef3d5c1f66127038a1be9b544d12491ea8
PenguinBoy13 <archie.anthony@blueyonder.co.uk>
2021-11-24 17:47:26 +0000

    Picture goes towards mouse on canvas

--- canvas-test/script.js
+++ canvas-test/script.js
@@ -1,17 +1,65 @@
 window.onload=function(){
     canvas = document.getElementById('game');
     ctx = canvas.getContext('2d');
-    setInterval(update,1000/60);
-    canvas.addEventListener('mousemove',function(mouse){
-        mouseX = mouse.clientX
-        mouseY = mouse.clientY
-        mouseClick = mouse.buttons
-    })
-}
+    setInterval(update,1000/120);
+    document.addEventListener('mousedown',function(mouse){
+        mouseX = mouse.clientX - 125;
+        mouseY = mouse.clientY - 45;
+        mouseClick = mouse.buttons;
+    });
+    document.addEventListener('mousemove',function(mouse){
+        mouseX = mouse.clientX - 125;
+        mouseY = mouse.clientY - 45;
+        mouseClick = mouse.buttons;
+    });
+    document.addEventListener('keypress',function(keyboard){
+        key = keyboard.key;
+    });
+    picture = document.getElementById('RetroCoder');
+    playerX = 10;
+    playerY = 10;
+};
 
 function update(){
-    if (mouseClick == 1){
-        ctx.fillStyle = "#FF0000";
-        ctx.fillRect(mouseX,mouseY,10,10);
-    }
-}
+    // if (mouseClick == 1){
+    //     ctx.fillStyle = "#FF0000";
+    //     ctx.fillRect(mouseX,mouseY,10,10);
+    // };
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    player = ctx.drawImage(picture, playerX, playerY, width="50", height="50");
+    // if (key == "w"){
+    //     key = 'none';
+    //     playerY -= 10
+    // };
+    // if (key == "a"){
+    //     key = 'none';
+    //     playerX -= 10
+    // };
+    // if (key == "s"){
+    //     key = 'none';
+    //     playerY += 10
+    // };
+    // if (key == "d"){
+    //     key = 'none';
+    //     playerX += 10
+    // };
+    diffX = mouseX - playerX;
+    diffY = mouseY - playerY;
+
+    // if (key == "w"){
+    //     key = 'none';
+    //     alert(diffX);
+    // };
+    if (diffX > 0){
+        playerX += 1;
+    };
+    if (diffX < 0){
+        playerX -= 1;
+    };
+    if (diffY > 0){
+        playerY += 1;
+    };
+    if (diffY < 0){
+        playerY -= 1;
+    };
+};
