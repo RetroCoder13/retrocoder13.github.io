8ad3c8d71af3f7d354885bd7d18e0ca31d8285b7
PenguinBoy13 <archie.anthony@blueyonder.co.uk>
2021-11-24 16:27:30 +0000

    Test touch screen element

--- canvas-test/script.js
+++ canvas-test/script.js
@@ -2,10 +2,15 @@ window.onload=function(){
     canvas = document.getElementById('game');
     ctx = canvas.getContext('2d');
     setInterval(update,1000/60);
-    canvas.addEventListener('mousemove',function(e){
-        mouseX = e.clientX
-        mouseY = e.clientY
-        mouseClick = e.buttons
+    canvas.addEventListener('mousemove',function(mouse){
+        mouseX = mouse.clientX
+        mouseY = mouse.clientY
+        mouseClick = mouse.buttons
+    })
+    canvas.addEventListener('touchmove',function(touch){
+        touchX = touch.pageX
+        touchY = touch.pageY
+        //mouseClick = e.buttons
     })
 }
 
@@ -14,4 +19,5 @@ function update(){
         ctx.fillStyle = "#FF0000";
         ctx.fillRect(mouseX,mouseY,10,10);
     }
+    document.write(pageX,pageY)
 }
