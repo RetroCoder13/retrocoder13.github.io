725be1f9c6bcc7ab2964e7181e1acc26fc13a067
PenguinBoy13 <archie.anthony@blueyonder.co.uk>
2021-12-04 16:49:39 +0000

    Added Color Picker for some reason

---
+++ color/script.js
@@ -0,0 +1,10 @@
+function update(){
+    var color = document.getElementById('color')
+    document.getElementById('body').style.backgroundColor = "#" + color.value;
+    color.style.backgroundColor = "#" + color.value;
+
+    if (color.value == ''){
+        document.getElementById('body').style.backgroundColor = "#000000";
+        color.style.backgroundColor = "#000000";
+    };
+};
