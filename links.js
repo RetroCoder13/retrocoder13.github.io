25cf2b28234d1de097146be82850e60b3cf2327a
PenguinBoy13 <archie.anthony@blueyonder.co.uk>
2022-01-16 16:10:16 +0000

    Made links easier to change

---
+++ links.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+    var buttons = document.getElementById('Buttons')
+    buttons.innerHTML = "<ul>\
+        <li>\
+            <a class=\"button\" href=\"https://retrocoder13.github.io\">Home</a>\
+        </li>\
+        <li>\
+            <a class=\"button\" href=\"https://retrocoder13.github.io/projects/\">Projects</a>\
+        </li>\
+        <li>\
+            <a class=\"button\" href=\"https://retrocoder13.github.io/request/\">Request</a>\
+        </li>\
+        <li>\
+            <a href=\"https://retrocoder.itch.io\" class=\"button\">itch.io</a>\
+        </li>\
+        <li>\
+            <a href=\"https://retrocoder13.github.io/credits/\" class=\"button\">Credits</a>\
+        </li>\
+    </ul>"
+}
