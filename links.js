e619a256bfc775eb652e61da85ea37457738376d
PenguinBoy13 <archie.anthony@blueyonder.co.uk>
2022-01-16 16:21:42 +0000

    itch.io and GitHub open in new tabs

--- links.js
+++ links.js
@@ -14,10 +14,10 @@ window.onload = function(){
             <a href=\"https://retrocoder13.github.io/credits/\" class=\"button\">Credits</a>\
         </li>\
         <li>\
-            <a href=\"https://retrocoder.itch.io\" class=\"button\">itch.io</a>\
+            <a href=\"https://retrocoder.itch.io\" class=\"button\" target=\"_blank\">itch.io</a>\
         </li>\
         <li>\
-            <a href=\"https://github.com/RetroCoder13\" class=\"button\">GitHub</a>\
+            <a href=\"https://github.com/RetroCoder13\" class=\"button\" target=\"_blank\">GitHub</a>\
         </li>\
     </ul>"
 }
