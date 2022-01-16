ab011b6d9d895c3d65dedcfe4a3f08d993034f13
PenguinBoy13 <archie.anthony@blueyonder.co.uk>
2022-01-16 16:12:25 +0000

    Added GitHub

--- links.js
+++ links.js
@@ -11,10 +11,13 @@ window.onload = function(){
             <a class=\"button\" href=\"https://retrocoder13.github.io/request/\">Request</a>\
         </li>\
         <li>\
+            <a href=\"https://retrocoder13.github.io/credits/\" class=\"button\">Credits</a>\
+        </li>\
+        <li>\
             <a href=\"https://retrocoder.itch.io\" class=\"button\">itch.io</a>\
         </li>\
         <li>\
-            <a href=\"https://retrocoder13.github.io/credits/\" class=\"button\">Credits</a>\
+            <a href=\"https://github.com/RetroCoder13\" class=\"button\">GitHub</a>\
         </li>\
     </ul>"
 }
