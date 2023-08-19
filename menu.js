ca38ebc3a90cfb9548ac90b1aa77e3a7296eeee2
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2023-08-19 19:34:36 +0100

    Updated code

--- menu.js
+++ menu.js
@@ -11,3 +11,22 @@ function toggleMenu(){
         document.getElementById('menu-button').style.animation = "pop-in .5s forwards"
     }
 }
+
+var menuData = document.createElement("div")
+menuData.id = "menu"
+menuData.className = "menu"
+menuData.innerHTML = `
+<div id="menu-button" class="menu-button">
+    <img src="assets/textures/menu.png" width="50" height="50" onclick="toggleMenu()">
+</div>
+<div id="menu-info" class="menu-info">
+    <h1>Links</h1>
+    <a href="https://retrocoder13.github.io/" id="link">Home</a>
+    <a href="https://retrocoder13.github.io/projects/" id="link">Projects</a>
+    <a href="https://retrocoder.itch.io/" id="link">itch.io</a>
+    <a href="https://github.com/RetroCoder13/" id="link">GitHub</a>
+</div>
+`
+window.onload = function(){
+    document.body.append(menuData)
+}
